42506097841ee3cc4425928bf31afddd746cd370
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-14 19:31:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,11 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
+  contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
-     end
-   end
- end
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
+    end
+  end
+end
 end
